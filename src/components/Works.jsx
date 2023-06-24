60fa24b3acab10ff4e90fe280d57d61d6d4af5f6
LuchoGiuliani <luciano.giuliani92@gmail.com>
2023-06-24 20:08:03 -0300

    cambios en spotify

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -65,13 +65,10 @@ const Works = () => {
         <p className={styles.sectionSubText}>playlist</p>
         <h2 className={styles.sectionHeadText}>Spotify.</h2>
 
-        <iframe
-          style="border-radius:12px"
+        <iframe className="rounded-xl"
           src="https://open.spotify.com/embed/playlist/1sjw7ma7rnYcYSQiG83oee?utm_source=generator&theme=0"
           width="100%"
           height="352"
-          frameBorder="0"
-          allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
